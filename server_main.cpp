a5319157d6e3af8938127f84b26e8b3417e2d35b
EddyGlezz <eddyglezz95@gmail.com>
2023-03-12 22:01:28 +0100

    import the path of the relevant qml modules

--- server_main.cpp
+++ server_main.cpp
@@ -9,17 +9,21 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
+	engine.addImportPath("qrc:/esterVtech.com/imports");
+    engine.addImportPath("_deps/mydesigns-build/");
+    engine.addImportPath("_deps/account-build/");
+    engine.addImportPath("_deps/nodeconection-build/");
+    engine.addImportPath("_deps/booking_model-build//");
 	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
+	qDebug()<<engine.importPathList();
 
-	const QUrl url(u"qrc:/binter/binter/qml/window_server.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                            &app, [url](QObject *obj, const QUrl &objUrl) {
-                            if (!obj && url == objUrl)
-                            QCoreApplication::exit(-1);
-                            }, Qt::QueuedConnection);
+    const QUrl url(u"qrc:/esterVtech.com/imports/server/qml/window.qml"_qs);
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+			&app, [url](QObject *obj, const QUrl &objUrl) {
+			if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+			}, Qt::QueuedConnection);
 
 	engine.load(url);
-
-
 	return app.exec();
 }
