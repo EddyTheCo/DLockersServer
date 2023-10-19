4f37aff2caec253a570f0762b786d6069fbc773d
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-10-19 19:01:48 +0200

    removed force linking and explicit link to the static module

--- server_main.cpp
+++ server_main.cpp
@@ -2,14 +2,12 @@
 #include <QQmlApplicationEngine>
 #include "Qrimageprovider.hpp"
 #include "mydesigns.hpp"
-#include "omqml.hpp"
 
 
 int main(int argc, char *argv[])
 {
-    auto foo=fooDesign::fooPrint(); //https://forum.qt.io/post/762513
-    foo=fooQtQrGen::fooPrint();
-    foo=fooOMQml::fooPrint();
+	auto foo=fooDesign::fooPrint(); //https://forum.qt.io/post/762513
+	foo=fooQtQrGen::fooPrint();
 
 	QGuiApplication app(argc, argv);
 
@@ -17,14 +15,8 @@ int main(int argc, char *argv[])
 	engine.addImportPath("qrc:/esterVtech.com/imports");
 	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
 	qDebug()<<engine.importPathList();
-    qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
-    const QUrl url(u"qrc:/esterVtech.com/imports/server/qml/window.qml"_qs);
-	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-			&app, [url](QObject *obj, const QUrl &objUrl) {
-			if (!obj && url == objUrl)
-			QCoreApplication::exit(-1);
-			}, Qt::QueuedConnection);
-
+	qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
+	const QUrl url(u"qrc:/esterVtech.com/imports/server/qml/window.qml"_qs);
 	engine.load(url);
 	return app.exec();
 }
